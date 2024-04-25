6fbf3ba626db494a14afeadf6e765445a2f4b865
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2024-04-25 17:24:22 +0200

    27.3 Testowanie controllerow - Zadanie: TaskControllerTest

--- src/test/java/com/crud/tasks/service/DbServiceTest.java
+++ src/test/java/com/crud/tasks/service/DbServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
